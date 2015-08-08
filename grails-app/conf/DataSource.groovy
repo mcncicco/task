dd3732daf40d06c80f25f5aa3622747bc67ea472
mcncicco <marcelocicco@gmail.com>
2015-08-08 15:32:59 -0300

    Banco
    Banco

--- grails-app/conf/DataSource.groovy
+++ grails-app/conf/DataSource.groovy
@@ -1,33 +1,4 @@
-dataSource {
-    pooled = true
-    jmxExport = true
-    driverClassName = "org.postgresql.Driver"
-    username = uri.userInfo.split(":")[0]
-        password = uri.userInfo.split(":")[1]
-}
-hibernate {
-    cache.use_second_level_cache = true
-    cache.use_query_cache = false
-//    cache.region.factory_class = 'org.hibernate.cache.SingletonEhCacheRegionFactory' // Hibernate 3
-    cache.region.factory_class = 'org.hibernate.cache.ehcache.SingletonEhCacheRegionFactory' // Hibernate 4
-    singleSession = true // configure OSIV singleSession mode
-    flush.mode = 'manual' // OSIV session flush mode outside of transactional context
-}
-
-// environment specific settings
-environments {
-    development {
-        dataSource {
-            dbCreate = "update" // one of 'create', 'create-drop', 'update', 'validate', ''
-            url = "jdbc:h2:file:devDb;MVCC=TRUE;LOCK_TIMEOUT=10000;DB_CLOSE_ON_EXIT=FALSE"
-        }
-    }
-    test 
-{        dataSource {
-            dbCreate = "update"
-            url = "jdbc:h2:mem:testDb;MVCC=TRUE;LOCK_TIMEOUT=10000;DB_CLOSE_ON_EXIT=FALSE"
-        }
-    }
+production {
     dataSource {
         dbCreate = "update"
         driverClassName = "org.postgresql.Driver"
