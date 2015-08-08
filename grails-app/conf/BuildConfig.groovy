90a58425011bf09d605742d0d793c52119989e04
mcncicco <marcelocicco@gmail.com>
2015-08-08 16:15:03 -0300

    conf
    conf

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -64,6 +64,9 @@ grails.project.dependency.resolution = {
         runtime ":database-migration:1.4.0"
         runtime ":jquery:1.11.1"
 
+        compile ':heroku:1.0.1'
+        compile ':cloud-support:1.0.8'
+
         // Uncomment these to enable additional asset-pipeline capabilities
         //compile ":sass-asset-pipeline:1.9.0"
         //compile ":less-asset-pipeline:1.10.0"
