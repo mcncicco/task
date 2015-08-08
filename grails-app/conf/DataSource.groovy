12fb6ab01d9fd55342b350c8b519e8f4bb10d7dd
mcncicco <marcelocicco@gmail.com>
2015-08-08 17:16:43 -0300

    teste
    teste

--- grails-app/conf/DataSource.groovy
+++ grails-app/conf/DataSource.groovy
@@ -1,13 +1,54 @@
-production {
-    dataSource {
-        dbCreate = "update"
-        driverClassName = "org.postgresql.Driver"
-        dialect = org.hibernate.dialect.PostgreSQLDialect
+dataSource {
+    pooled = true
+    jmxExport = true
+    driverClassName = "org.postgresql.Driver"
+    username = uri.userInfo.split(":")[0]
+	        password = uri.userInfo.split(":")[1]
+	        dialect = org.hibernate.dialect.PostgreSQLDialect
 
-        uri = new URI(System.env.DATABASE_URL?:"postgres://test:test@localhost/test")
+	        uri = new URI(System.env.DATABASE_URL?:"postgres://test:test@localhost/test")
 
-        url = "jdbc:postgresql://" + uri.host + ":" + uri.port + uri.path
-        username = uri.userInfo.split(":")[0]
-        password = uri.userInfo.split(":")[1]
-    }
+	        url = "jdbc:postgresql://" + uri.host + ":" + uri.port + uri.path
 }
+
+
+ 
+hibernate {
+    cache.use_second_level_cache = true
+    cache.use_query_cache = false
+//    cache.region.factory_class = 'org.hibernate.cache.SingletonEhCacheRegionFactory' // Hibernate 3
+    cache.region.factory_class = 'org.hibernate.cache.ehcache.SingletonEhCacheRegionFactory' // Hibernate 4
+    singleSession = true // configure OSIV singleSession mode
+    flush.mode = 'manual' // OSIV session flush mode outside of transactional context
+}
+
+// environment specific settings
+environments {
+    development {
+        dataSource {
+            dbCreate = "update" // one of 'create', 'create-drop', 'update', 'validate', ''
+            url = "jdbc:h2:file:devDb;MVCC=TRUE;LOCK_TIMEOUT=10000;DB_CLOSE_ON_EXIT=FALSE"
+        }
+    }
+    test 
+{        dataSource {
+            dbCreate = "update"
+            url = "jdbc:h2:mem:testDb;MVCC=TRUE;LOCK_TIMEOUT=10000;DB_CLOSE_ON_EXIT=FALSE"
+        }
+    }
+    
+	production {
+	    dataSource {
+	        dbCreate = "update"
+	        driverClassName = "org.postgresql.Driver"
+	        dialect = org.hibernate.dialect.PostgreSQLDialect
+	         System.out.println(System.env.DATABASE_URL);
+
+	        uri = new URI(System.env.DATABASE_URL?:"postgres://test:test@localhost/test")
+
+	        url = "jdbc:postgresql://" + uri.host + ":" + uri.port + uri.path
+	        username = uri.userInfo.split(":")[0]
+	        password = uri.userInfo.split(":")[1]
+	    }
+	}
+	}
